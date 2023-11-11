aa4f6d7a4a536ea71b0ae7cecc2577f7a5f29deb
lukmay <86775313+lukmay@users.noreply.github.com>
2023-11-11 06:22:51 +0100

    add security config

---
+++ Backend_API/hackathon/hackathon/src/main/java/com/hackathon/hackathon/dto/SensorAcceptanceDTO.java
@@ -0,0 +1,22 @@
+package com.hackathon.hackathon.dto;
+
+import com.hackathon.hackathon.model.SensorType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SensorAcceptanceDTO {
+
+    private Long sensorID;
+
+    private SensorType type;
+
+    private boolean available;
+
+    private boolean accepted;
+
+    private int updateRateInMS;
+
+    private Long roomID;
+}
