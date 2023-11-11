83d6c3a3e39c23feedbd3d8b12d32b48c7bb41a3
Moritz Geier <moritz.geier@proton.me>
2023-11-11 06:05:08 +0100

    Made activityled time variable

--- Demo/AcitivtyLED.ino
+++ Demo/AcitivtyLED.ino
@@ -1,10 +1,10 @@
-void activityLED(int pin)
+void activityLED(int pin, unsigned long interval)
 {
     static bool state = false;
     static unsigned long lastStateChange = millis();
 
     const unsigned long currentMillis = millis();
-    if (lastStateChange + 1000 < currentMillis)
+    if (lastStateChange + interval < currentMillis)
     {
         if (state)
         {
