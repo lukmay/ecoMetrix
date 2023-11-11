a71354af1d7686938a016a766fcc4fd8629803e4
lukmay <86775313+lukmay@users.noreply.github.com>
2023-11-11 08:49:06 +0100

    add secur conf

--- Backend_API/hackathon/hackathon/src/main/java/com/hackathon/hackathon/config/WebSecurityConfig.java
+++ Backend_API/hackathon/hackathon/src/main/java/com/hackathon/hackathon/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,19 +16,14 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class WebSecurityConfig {
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
-                );
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
-
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
